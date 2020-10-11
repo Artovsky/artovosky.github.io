bd32d5099a2da63fb9ac7e17963dc7c15f6eef4e
Artovsky <arlok2211@gmail.com>
2020-10-11 12:53:52 +0300

    upload files
    12345

---
+++ Artovsky/app.js
@@ -0,0 +1,42 @@
+// let dict = {
+
+// 1: '16 лет',
+// 2: '13 лет',
+// 3: '54 года',
+// 123: 321,
+// 911: "1+police"
+// }
+
+// console.log(dict[911])
+
+
+
+
+
+
+
+
+
+let numArr = [10,9,1,2,99,101,66,33,28,54]
+            // [1,2,9,10,28,33,54,66,99,101]
+console.log(numArr)
+numArr[10]= 15
+console.log(numArr)
+console.log(numArr.length)
+alert('helloworld')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
